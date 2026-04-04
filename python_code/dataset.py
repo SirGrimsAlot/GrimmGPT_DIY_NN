0c084d33eee5dd0359c558240d09c48ddc88466a
SirGrimsAlot <lithium.grim@gmail.com>
2026-04-04 15:29:13 +0200

    Fix: fixed mistake in logic for getting a item from the dataset

--- python_code/dataset.py
+++ python_code/dataset.py
@@ -1,21 +1,24 @@
 import pandas as pd
 import torch
 import os
-import cv2
+from PIL import Image
 
 
 class IntelDataSet(torch.utils.data.Dataset):
-    def __init__(self, csv_file, root_image_dir="", transform=None):
-        self.annotation_df = pd.read_csv(csv_file)
-        self.root_image_dir = root_image_dir
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.annotations = pd.read_csv(csv_file)
+        self.root_dir = root_dir
         self.transform = transform
         
     def __len__(self):
-        return self.len(self.annotation_df)
+        return self.len(self.annotations) #get dataset size
     
     def __getitem__(self, index):
-        image_path = os.path.join(self.root_image_dir, self.annotation_df.iloc[index, 0]) #take actual path
-        image = cv2.imread(image_path)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        class_index = self.annotation_df.iloc[index, 1]
-        return image, class_index
+        image_path = os.path.join(self.root_dir, self.annotations.iloc[index, 0]) #get image path by taking the image name of curr image to get  
+        image = Image.open(image_path).convert("RGB")
+        image_label = int(self.annotations.iloc[index, 1]) #get image label by taking second column of the curr row 
+        
+        if self.transform:
+            image = self.transform(image) #apply transform on image if transform is given 
+
+        return image, image_label
